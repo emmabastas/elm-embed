9023b3f5678d0b62ca12ceaf98414775c46c6a6b
Emma Bastås <emma.bastas@protonmail.com>
2021-01-19 18:24:23 +0100

    Remove some code n stuff

--- terminal/src/Main.hs
+++ terminal/src/Main.hs
@@ -15,13 +15,8 @@ import qualified Elm.Version as V
 import Terminal
 import Terminal.Helpers
 
-import qualified Bump
-import qualified Diff
 import qualified Init
-import qualified Install
 import qualified Make
-import qualified Publish
-import qualified Repl
 
 
 
@@ -31,13 +26,8 @@ import qualified Repl
 main :: IO ()
 main =
   Terminal.app intro outro
-    [ repl
-    , init
+    [ init
     , make
-    , install
-    , bump
-    , diff
-    , publish
     ]
 
 
@@ -90,51 +80,15 @@ init =
 
 
 
--- REPL
-
-
-repl :: Terminal.Command
-repl =
-  let
-    summary =
-      "Open up an interactive programming session. Type in Elm expressions\
-      \ like (2 + 2) or (String.length \"test\") and see if they equal four!"
-
-    details =
-      "The `repl` command opens up an interactive programming session:"
-
-    example =
-      reflow
-        "Start working through <https://guide.elm-lang.org> to learn how to use this!\
-        \ It has a whole chapter that uses the REPL for everything, so that is probably\
-        \ the quickest way to get started."
-
-    replFlags =
-      flags Repl.Flags
-        |-- flag "interpreter" interpreter "Path to a alternate JS interpreter, like node or nodejs."
-        |-- onOff "no-colors" "Turn off the colors in the REPL. This can help if you are having trouble reading the values. Some terminals use a custom color scheme that diverges significantly from the standard ANSI colors, so another path may be to pick a more standard color scheme."
-  in
-  Terminal.Command "repl" (Common summary) details example noArgs replFlags Repl.run
-
-
-interpreter :: Parser String
-interpreter =
-  Parser
-    { _singular = "interpreter"
-    , _plural = "interpreters"
-    , _parser = Just
-    , _suggest = \_ -> return []
-    , _examples = \_ -> return ["node","nodejs"]
-    }
-
-
-
 -- MAKE
 
 
 make :: Terminal.Command
 make =
   let
+    summary =
+      "The `make` command generates Elm code from code inside `elm-generates-scripts`:"
+
     details =
       "The `make` command generates Elm code from code inside `elm-generates-scripts`:"
 
@@ -146,132 +100,8 @@ make =
         |-- flag "interpreter" Make.interpreterPath "Path to a alternate JS interpreter, like node or nodejs."
         |-- flag "report" Make.reportType "You can say --report=json to get error messages as JSON. This is only really useful if you are an editor plugin. Humans should avoid it!"
   in
-  Terminal.Command "make" Uncommon details example noArgs makeFlags Make.run
+  Terminal.Command "make" (Common summary) details example noArgs makeFlags Make.run
 
-
-
--- INSTALL
-
-
-install :: Terminal.Command
-install =
-  let
-    details =
-      "The `install` command fetches packages from <https://package.elm-lang.org> for\
-      \ use in your project:"
-
-    example =
-      stack
-        [ reflow
-            "For example, if you want to get packages for HTTP and JSON, you would say:"
-        , P.indent 4 $ P.green $ P.vcat $
-              [ "elm install elm/http"
-              , "elm install elm/json"
-              ]
-        , reflow
-            "Notice that you must say the AUTHOR name and PROJECT name! After running those\
-            \ commands, you could say `import Http` or `import Json.Decode` in your code."
-        , reflow
-            "What if two projects use different versions of the same package? No problem!\
-            \ Each project is independent, so there cannot be conflicts like that!"
-        ]
-
-    installArgs =
-      oneOf
-        [ require0 Install.NoArgs
-        , require1 Install.Install package
-        ]
-  in
-  Terminal.Command "install" Uncommon details example installArgs noFlags Install.run
-
-
-
--- PUBLISH
-
-
-publish :: Terminal.Command
-publish =
-  let
-    details =
-      "The `publish` command publishes your package on <https://package.elm-lang.org>\
-      \ so that anyone in the Elm community can use it."
-
-    example =
-      stack
-        [ reflow
-            "Think hard if you are ready to publish NEW packages though!"
-        , reflow
-            "Part of what makes Elm great is the packages ecosystem. The fact that\
-            \ there is usually one option (usually very well done) makes it way\
-            \ easier to pick packages and become productive. So having a million\
-            \ packages would be a failure in Elm. We do not need twenty of\
-            \ everything, all coded in a single weekend."
-        , reflow
-            "So as community members gain wisdom through experience, we want\
-            \ them to share that through thoughtful API design and excellent\
-            \ documentation. It is more about sharing ideas and insights than\
-            \ just sharing code! The first step may be asking for advice from\
-            \ people you respect, or in community forums. The second step may\
-            \ be using it at work to see if it is as nice as you think. Maybe\
-            \ it ends up as an experiment on GitHub only. Point is, try to be\
-            \ respectful of the community and package ecosystem!"
-        , reflow
-            "Check out <https://package.elm-lang.org/help/design-guidelines> for guidance on how to create great packages!"
-        ]
-  in
-  Terminal.Command "publish" Uncommon details example noArgs noFlags Publish.run
-
-
-
--- BUMP
-
-
-bump :: Terminal.Command
-bump =
-  let
-    details =
-      "The `bump` command figures out the next version number based on API changes:"
-
-    example =
-      reflow
-        "Say you just published version 1.0.0, but then decided to remove a function.\
-        \ I will compare the published API to what you have locally, figure out that\
-        \ it is a MAJOR change, and bump your version number to 2.0.0. I do this with\
-        \ all packages, so there cannot be MAJOR changes hiding in PATCH releases in Elm!"
-  in
-  Terminal.Command "bump" Uncommon details example noArgs noFlags Bump.run
-
-
-
--- DIFF
-
-
-diff :: Terminal.Command
-diff =
-  let
-    details =
-      "The `diff` command detects API changes:"
-
-    example =
-      stack
-        [ reflow
-            "For example, to see what changed in the HTML package between\
-            \ versions 1.0.0 and 2.0.0, you can say:"
-        , P.indent 4 $ P.green $ "elm diff elm/html 1.0.0 2.0.0"
-        , reflow
-            "Sometimes a MAJOR change is not actually very big, so\
-            \ this can help you plan your upgrade timelines."
-        ]
-
-    diffArgs =
-      oneOf
-        [ require0 Diff.CodeVsLatest
-        , require1 Diff.CodeVsExactly version
-        , require2 Diff.LocalInquiry version version
-        , require3 Diff.GlobalInquiry package version version
-        ]
-  in
-  Terminal.Command "diff" Uncommon details example diffArgs noFlags Diff.run
 
 
 
